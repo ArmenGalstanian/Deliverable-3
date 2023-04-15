855d5afb6651e6c803fbf4c84223b85b65019521
ArmenGalstanian <77305092+ArmenGalstanian@users.noreply.github.com>
2023-04-14 23:26:44 -0400

    Add files via upload

---
+++ Deliverable3/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author armen
+ *
+ */
+module jfreeChartTesting {
+	requires jfreechart;
+	requires jcommon;
+	requires java.desktop;
+	requires junit;
+	requires jdk.incubator.vector;
+}
---
+++ Deliverable3/src/project/dataEntry.java
@@ -0,0 +1,11 @@
+package project;
+
+public class dataEntry {
+
+    public int ts;
+    public String months;
+
+    public String iterations;
+
+
+}
